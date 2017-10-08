b0e863161789098408a221cd9099d808a2316308
Richard Woods <rm-woods@hotmail.com>
2017-10-08 12:58:43 -0400

    More particle implementation

--- Pong/Graphics5/ParticleEmitter.hpp
+++ Pong/Graphics5/ParticleEmitter.hpp
@@ -7,21 +7,72 @@
 class ParticleEmitter
 {
 public:
-	size_t poolsize;
+	size_t poolsize; // max particles
 	std::vector<Particle *> v;
-	sf::Vector2f acceleration;
-	sf::Vector2f direction;
 
-	// delta time, render window to draw to
-	void update(float dt, sf::RenderWindow & window)
+	float duration; // how long the particle system exists
+	bool looping; // restart after duration?
+	
+	size_t emission; // particles per second
+
+	// selects random velocity between low and high
+	float startVelocityLow;
+	float startVelocityHigh;
+
+	float maxVelocity;
+	sf::Vector2f acceleration;
+
+	// Selects value at birth of particle between two degrees
+	float directionLow;
+	float directionHigh;
+	
+	// How long each particle lives, random val between low and high
+	float lifeLow;
+	float lifeHigh;
+
+	sf::Color startColor;
+	sf::Color endColor;
+
+	ParticleEmitter(sf::RectangleShape const& shape, sf::Vector2f accel, size_t maxsize)
+	{
+		poolsize = maxsize;
+		v = std::vector<Particle *>(poolsize);
+		for (size_t i = 0; i < poolsize; ++i)
+		{
+			Particle* temp = new Particle();
+			sf::Vector2f tv = sf::Vector2f();
+			temp->velocity = tv;
+			temp->setShape(shape);
+			v[i] = temp;
+		}
+	}
+
+	// render window to draw to
+	void draw(sf::RenderWindow & window)
 	{
 		for (size_t i = 0; i < poolsize; ++i)
 		{
-			Particle *p = v[i];
-			if (p->alive)
+			window.draw(*v[i]);
+		}
+	}
+
+	// delta time
+	void update(float dt)
+	{
+		for (size_t i = 0; i < poolsize; ++i)
+		{
+			if (v[i]->alive)
 			{
-				window.draw(p->update(dt, acceleration));
+				v[i]->update(dt, acceleration);
 			}
+		}
+	}
+
+	~ParticleEmitter()
+	{
+		for (size_t i = 0; i < poolsize; ++i)
+		{
+			delete v[i];
 		}
 	}
 };
--- Pong/Graphics5/particle.hpp
+++ Pong/Graphics5/particle.hpp
@@ -16,12 +16,24 @@ public:
 		velocity = sf::Vector2f(0, 0);
 	}
 
-	Particle const& update(float dt, sf::Vector2f a)
+	void setShape(sf::RectangleShape const& s)
+	{
+		setOrigin(s.getOrigin());
+		setScale(s.getScale());
+		setSize(s.getSize());
+		setPosition(s.getPosition());
+		setRotation(s.getRotation());
+		setFillColor(s.getFillColor());
+		setOutlineColor(s.getOutlineColor());
+		setOutlineThickness(s.getOutlineThickness());
+		setTexture(s.getTexture());
+		setTextureRect(s.getTextureRect());
+	}
+
+	void update(float dt, sf::Vector2f a)
 	{
 		velocity = sf::Vector2f(a.x * dt, a.y * dt);
 		sf::Vector2f disp = sf::Vector2f(velocity.x * dt, velocity.y * dt);
 		move(disp);
-
-		return *this;
 	}
 };
